47ba5db881e6633b036b4cbc9029ab5b636f6527
YehChenHsuan <atax1022@gmail.com>
2026-09-11 15:51:15 +0800

    Initial commit: P7_skating_bunny_game

--- js/sentences_map.js
+++ js/sentences_map.js
@@ -1,4 +1,4 @@
-{
+window.SENTENCES_AUDIO_MAP = {
   "Collect all carrots starting with ar": "assets/audios/sentences/bunny_collect_ar.mp3",
   "Find words starting with ar": "assets/audios/sentences/bunny_find_ar.mp3",
   "Find words starting with letter ar": "assets/audios/sentences/bunny_find_let_ar.mp3",
@@ -13,4 +13,4 @@
   "Be quiet when reading a story in the library. ... What is the classroom rule?": "assets/audios/sentences/bunny_act_0bc6ff01.mp3",
   "Use your yellow ruler to measure the long line. ... What tool do you use to measure?": "assets/audios/sentences/bunny_act_712005ca.mp3",
   "Open your book and look at the lesson. ... What should you open?": "assets/audios/sentences/bunny_act_30c69f2e.mp3"
-}
+};
